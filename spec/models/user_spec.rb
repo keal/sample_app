afb497e38ddaac5449dd66124924ccb8de7946ca
田中 貴章 <tanakatakaaki@tanaka-no-MacBook-Pro.local>
2012-08-22 18:56:00 +0900

    list6.1.5 commit

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe User do
+
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+end
