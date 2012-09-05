5f8081c4b29dfffb0860b40570e6ea3e14b8218a
田中 貴章 <tanakatakaaki@tanaka-no-MacBook-Pro.local>
2012-09-05 11:11:08 +0900

    Add user following

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
 			"#{base_title} | #{page_title}"
 		end
 	end
+
+	def show_delete_link object_of_micropost
+		if current_user?(object_of_micropost.user)
+    	link_to "delete", object_of_micropost, method: :delete,
+                                     data: { confirm: "You sure?" },
+                                     title: object_of_micropost.content 
+  		end 
+  	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,10 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) } 
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -162,5 +166,37 @@ describe User do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
     end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+        end
+      end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
   end
 end
